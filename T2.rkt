7fb58e5dff04220e1c900a77d9161a16b476769c
Julio Yáñez <julio.yanez@ug.uchile.cl>
2026-09-15 18:30:11 -0300

    feat(T2): implemented parse-prop

---
+++ T2.rkt
@@ -0,0 +1,183 @@
+#lang play
+
+#|
+Nombre: Julio Yáñez
+¿Utilizó Whiteboard Policy? (SI o NO): NO
+En caso afirmativo, ¿con quién?: _
+¿en qué ejercicio(s)?: _
+|#
+
+;;------------ ;;
+;;==== P1 ==== ;;
+;;------------ ;;
+
+#| Parte A |#
+
+#|
+<prop> ::= (tt)
+         | (ff)
+         | (p-id <id>)
+         | (p-not <prop>)
+         | (p-and (<prop> <prop>+))
+         | (p-or (<prop> <prop>+))
+         | (p-with <id> <prop> <prop>)
+|#
+
+(deftype Prop
+  (tt)
+  (ff)
+  (p-id x)
+  (p-not p)
+  (p-and p-mult)
+  (p-or p-mult)
+  (p-with x val p))
+
+#| Parte B |#
+
+#|
+<s-prop> ::= true
+           | false
+           | <id>
+	   | (list 'not <s-prop>) 
+	   | (list 'and <s-prop> <s-prop>+)
+	   | (list 'or <s-prop> <s-prop>+)
+	   | (list 'with <id> <s-prop> <s-prop>)
+|#
+
+;; parse-prop :: <s-prop> -> Prop
+;; Parsea programas escritos en sintaxis concreta a progamas escritos en sintaxis abstracta.
+(define (parse-prop p)
+  (match p
+    [(? symbol? v) #:when (eq? v 'true) (tt)]
+    [(? symbol? v) #:when (eq? v 'false) (ff)]
+    [(? symbol? x) (p-id x)]
+    [(list 'not prop) (p-not (parse-prop prop))]
+    [(list 'and props ...) (if (<= 2 (length props))
+			       (p-and (map parse-prop props))
+			       (error "parse-prop: and expects at least two operands."))]
+    [(list 'or props ...) (if (<= 2 (length props))
+			      (p-or (map parse-prop  props))
+			      (error "parse-prop: or expects at least two operands."))]
+    [(list 'with (? symbol? id) value prop) 
+     (p-with id (parse-prop value) (parse-prop prop))]
+    ))
+
+#| Parte C |#
+
+;; p-subst :: Prop Symbol Prop -> Prop
+(define (p-subst expr x val) '???)
+
+
+#| Parte D |#
+
+;; p-eval :: Prop -> Boolean
+(define (p-eval p) '???)
+
+
+
+;;------------ ;;
+;;==== P2 ==== ;;
+;;------------ ;;
+
+#| Parte A y C |#
+
+#|
+<expr> ::= <number>
+         | (id <id>)
+         | (add <expr> <expr>)
+         | (mul <expr> <expr>)
+         | (if0 <expr> <expr> <expr>)
+         | (with <id> <expr> <expr>)
+         ...
+|#
+
+(deftype Expr
+  (num n)
+  (id x)
+  (add l r)
+  (mul l r)
+  (if0 c t f)
+  (with x v exp)
+  ;...
+  )
+
+#|
+<s-expr> ::= <number>
+           | <symbol>
+           | (list + <s-expr> <s-expr>)
+           | (list * <s-expr> <s-expr>)
+           | (list if0 <s-expr> <s-expr> <s-expr>)
+           | (list with <symbol> <s-expr> <s-expr>)
+           ...
+|#
+
+;; parser :: <s-expr> -> Expr
+(define (parser expr)
+  (match expr
+    [x #:when (number? x) (num x)]
+    [x #:when (symbol? x) (id x)]
+    [(list '+ a b) (add (parser a) (parser b))]
+    [(list '* a b) (mul (parser a) (parser b))]
+    [(list 'if0 c t f) (if0 (parser c) (parser t) (parser f))]
+    [(list 'with x v exp) (with x (parser v) (parser exp))]
+    ; ...
+    ))
+
+#| Parte B y C|#
+
+(deftype EValue
+  (numV n)
+  ;...
+  )
+
+;; numV+ :: EValue EValue -> EValue
+(define (numV+ l r)
+  (match (cons l r)
+    [(cons (numV x) (numV y)) (numV (+ x y))]
+    [_ (error 'numV+ "Wrong operands")]))
+
+;; numV* :: EValue EValue -> EValue
+(define (numV* l r)
+  (match (cons l r)
+    [(cons (numV x) (numV y)) (numV (* x y))]
+    [_ (error 'numV* "Wrong operands")]))
+
+;; is-zero-numV? :: EValue -> Boolean
+(define (is-zero-numV? x)
+  (match x
+    [(numV n) (zero? n)]
+    [_ (error 'is-zero-numV? "Wrong operand")]))
+
+(deftype Env
+  (mtEnv)
+  (xtEnv x v p-env))
+
+;; extend :: Symbol EValue Env -> Env
+(define (extend x v prev-env)
+  (xtEnv x v prev-env))
+
+;; lookup :: Symbol Env -> EValue
+(define (lookup x env)
+  (match env
+    [(mtEnv) (error 'lookup "Variable not found.")]
+    [(xtEnv y v prev) (if (equal? x y)
+                          v
+                          (lookup x prev))]))
+
+;; interp :: Expr Env -> EValue
+(define (interp expr env) '???)
+
+
+#| Parte C |#
+
+;; currying :: EValue -> EValue
+
+;; uncurrying :: EValue -> EValue
+
+;; swapping :: EValue -> EValue
+
+
+#| Parte D |#
+
+;; run :: <s-expr> -> EValue
+(define (run expr) '???)
--- test.rkt
+++ test.rkt
@@ -34,7 +34,7 @@ Ejercicio 1
 (test (p-eval (tt)) #t)
 (test (p-eval (ff)) #f)
 (test/exn (p-eval (p-id 'x)) "p-eval: Free variable.")
-(test/exn (p-eval (p-with 'x (ff) (p-and (list 'y 'x)))) "p-eval: Free variable.")
+(test/exn (p-eval (p-with 'x (ff) (p-and (list (p-id 'y) (p-id 'x))))) "p-eval: Free variable.")
 (test/exn (p-eval (p-subst (p-or (list (ff) 'y)) 'x (tt))) "p-eval: Free variable.")
 (test (p-eval (p-and (list (tt) (ff)))) #f)
 (test (p-eval (p-or (list (tt) (ff)))) #t)
