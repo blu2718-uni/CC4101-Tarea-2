c8e995e5d81412d6ec007b450519574e87d37a41
Julio Yáñez <julio.yanez@ug.uchile.cl>
2026-09-15 14:16:03 -0300

    feat(test): added testing for the section b) of exercise 1

---
+++ test.rkt
@@ -0,0 +1,23 @@
+#lang play
+
+#|
+Nombre: Julio Yáñez
+|#
+
+#|
+Ejercicio 1
+|#
+
+;; b)
+
+(test (parse-prop 'true) (tt))
+(test (parse-prop 'false) (ff))
+(test (parse-prop 'x) (p-id 'x))
+(test (parse-prop '(not true)) (p-not (tt)))
+(test/exn (parse-prop '(and true)) "parse-prop: and expects at least two operands.")
+(test/exn (parse-prop '(or)) "parse-prop: or expects at least two operands.")
+(test (parse-prop '(and true false true)) (p-and (list (tt) (ff) (tt))))
+(test (parse-prop '(or false true true)) (p-or (list (ff) (tt) (tt))))
+(test (parse-prop '(with x false (and true x))) (p-with 'x (ff) (p-and (list (tt) (p-id 'x)))))
+
+(require "T2.rkt")
